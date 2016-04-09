2d2cfe762f6b77a115825cab670b44f16ccaf9f5
Andrius Semionovas <andrius.semionovas@vinted.com>
2016-04-09 20:01:11 +0300

    Add choices

---
+++ main/src/main/java/lt/neworld/randomdecision/chooses/Choice.kt
@@ -0,0 +1,21 @@
+package lt.neworld.randomdecision.chooses
+
+/**
+ * @author neworld
+ * @since 09/04/16
+ */
+data class Choice(
+        val title: String,
+        val weight: Int,
+        val children:MutableList<Choice> = arrayListOf()
+) {
+    companion object {
+        fun of(line: String): Choice {
+            val pieces = line.split("\\W+".toRegex(), limit = 2)
+            if (pieces.size < 2) {
+                throw IllegalArgumentException("Format must be: %d %s")
+            }
+            return Choice(pieces.last(), pieces.first().toInt())
+        }
+    }
+}
---
+++ main/src/test/java/lt/neworld/randomdecision/chooses/BuilderTest.kt
@@ -0,0 +1,92 @@
+package lt.neworld.randomdecision.chooses
+
+import org.junit.Test
+import java.io.StringReader
+import kotlin.test.assertEquals
+
+/**
+ * @author neworld
+ * @since 09/04/16
+ */
+class BuilderTest {
+    @Test
+    fun testBuild_empty_defaultChoice() {
+        val input = StringReader("")
+
+        val actual = Builder("foo", input).build()
+
+        assertEquals(Choice("foo", 1), actual)
+    }
+
+    @Test
+    fun testBuild_withOneChoice_oneChild() {
+        val input = StringReader("2 bar")
+
+        val actual = Builder("foo", input).build()
+
+        val expected = Choice("foo", 1, arrayListOf(Choice("bar", 2)))
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testBuild_notTrimmedInput_success() {
+        val input = StringReader("   3 bar   ")
+
+        val actual = Builder("foo", input).build()
+
+        val expected = Choice("foo", 1, arrayListOf(Choice("bar", 3)))
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testBuild_emptyLine_default() {
+        val input = StringReader(" ")
+
+        val actual = Builder("foo", input).build()
+
+        assertEquals(Choice("foo", 1), actual)
+    }
+
+    @Test
+    fun testBuild_withTwoChoices_twoChildren() {
+        val input = StringReader("""
+            2 foo
+            3 bar
+        """)
+
+        val actual = Builder("foo", input).build()
+
+        val children = arrayListOf(Choice("foo", 2), Choice("bar", 3))
+        val expected = Choice("foo", 1, children)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testBuild_emptyListOfChildren() {
+        val input = StringReader("""
+            1 bar >
+            END
+        """)
+
+        val actual = Builder("foo", input).build()
+
+        val children = arrayListOf(Choice("bar", 1))
+        val expected = Choice("foo", 1, children)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testBuild_childOfChild() {
+        val input = StringReader("""
+            1 bar >
+                2 bar
+            END
+        """)
+
+        val actual = Builder("foo", input).build()
+
+        val children = arrayListOf(Choice("bar", 1, arrayListOf(Choice("bar", 2))))
+        val expected = Choice("foo", 1, children)
+        assertEquals(expected, actual)
+    }
+}
