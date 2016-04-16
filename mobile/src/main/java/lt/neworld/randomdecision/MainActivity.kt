192b7dd25db691082216098b620e01f3539008a9
Andrius Semionovas <andrius.semionovas@vinted.com>
2016-04-16 21:51:59 +0300

    Add chooser

--- mobile/src/main/java/lt/neworld/randomdecision/MainActivity.kt
+++ mobile/src/main/java/lt/neworld/randomdecision/MainActivity.kt
@@ -1,12 +1,33 @@
 package lt.neworld.randomdecision
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_main_settings -> openSettings().let { true }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun openSettings() {
+        val intent = Intent(this, SettingsActivity::class.java)
+        startActivity(intent)
     }
 }
